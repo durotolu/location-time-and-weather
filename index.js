15ae9fee8a0fa3a044f57ca2d859a90137a8c5a7
Modurotolu Olokode <durolawk@gmail.com>
2020-07-04 20:53:18 +0100

    activated API request

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const request = require("request");
 
 const location = process.argv[2];
 
+const wetherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.KEY}`;
 
 const getTime = (timezone) => {
   const currentDate = new Date();
@@ -11,3 +12,17 @@ const getTime = (timezone) => {
   console.log(currentTime);
   console.log(currentDate.getTimezoneOffset());
 }
+
+request (wetherUrl, (error, response, body) => {
+  try {
+    const result = JSON.parse(body);
+    const time = getTime(result.timezone);
+    console.log(result)
+    console.log(time)
+  } catch {
+    const error = JSON.parse(error);
+    if (err == null) {
+      console.log('here')
+    }
+  }
+})
