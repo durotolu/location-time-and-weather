7cd2f32c1866a66e71787922d8ce3465e7b8e121
Modurotolu Olokode <durolawk@gmail.com>
2020-07-05 22:45:01 +0100

    handled error messages and result data

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const request = require("request");
 
 const location = process.argv[2];
 
+if (!location) {
+  console.log("Kindly provide location(s)")
+  return;
+}
+
 const wetherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.KEY}`;
 
 const getTime = (timezone) => {
@@ -21,11 +26,22 @@ request (wetherUrl, (error, response, body) => {
   try {
     const result = JSON.parse(body);
     const time = getTime(result.timezone);
-    console.log(time);
+
+    if (result.cod !== '200') {
+      console.log(result.message);
+      return;
+    } else {
+      const name = result.name
+      const temperature = result.main.temp
+      console.log({
+        temp: result.n,
+      });
+    }
   } catch {
-    const err = JSON.parse(error);
-    if (err == null) {
+    const errorMsg = JSON.parse(error);
+    if (errorMsg == null) {
       console.log('here')
     }
+    console.log('gthing')
   }
 })
