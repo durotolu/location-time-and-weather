8036f863c66961278b3dba609f2c65daac7defee
Modurotolu Olokode <durolawk@gmail.com>
2020-07-09 00:38:42 +0100

    wrote getTimeFromTimezone test using local time

--- index.spec.js
+++ index.spec.js
@@ -3,11 +3,17 @@ const axios = require('axios');
 // const getWeatherInfo = require('./index');
 const helpers = require('./helpers');
 
-jest.mock("axios");
-// const mockedAxios = axios as jest.mockedAxios
-// axios.get.mockResolvedValue(resp);
+// jest.mock("axios");
+
 
 test('formatInputs returns array of locations', async () => {
   const response = helpers.formatInputs([ 'Lagos,New', 'York,New', 'Zealand,London,Texas,New', 'Jersey' ]);
-  expect(response).toEqual(["Lagos", "New York", "New Zealand", "London", "Texas", "New Jersey"])
+  expect(response).toEqual(["Lagos", "New York", "New Zealand", "London", "Texas", "New Jersey"]);
+});
+
+test('getTimeFromTimezone returns the current time based on timezone', async () => {
+  const currentDate = new Date();
+  const localTimezone = currentDate.getTimezoneOffset() * -60;
+  const localTime = helpers.getTimeFromTimezone(localTimezone);
+  expect(localTime).toEqual(currentDate.toLocaleTimeString());
 });
