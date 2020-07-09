61d149b73fdc6b3727c796a39f7767bde1fdb378
Modurotolu Olokode <durolawk@gmail.com>
2020-07-09 16:28:04 +0100

    wrote getTimeFromTimezone test

--- index.spec.js
+++ index.spec.js
@@ -1,19 +1,23 @@
 const request = require('supertest');
 const axios = require('axios');
-// const getWeatherInfo = require('./index');
 const helpers = require('./helpers');
 
-// jest.mock("axios");
+jest.mock("axios");
 
-
-test('formatInputs returns array of locations', async () => {
+test('formatInputs returns array of locations', () => {
   const response = helpers.formatInputs([ 'Lagos,New', 'York,New', 'Zealand,London,Texas,New', 'Jersey' ]);
   expect(response).toEqual(["Lagos", "New York", "New Zealand", "London", "Texas", "New Jersey"]);
 });
 
-test('getTimeFromTimezone returns the current time based on timezone', async () => {
+test('getTimeFromTimezone returns the current time based on timezone', () => {
   const currentDate = new Date();
   const localTimezone = currentDate.getTimezoneOffset() * -60;
   const localTime = helpers.getTimeFromTimezone(localTimezone);
   expect(localTime).toEqual(currentDate.toLocaleTimeString());
+});
+
+test('getWeatherInfo returns undefined without proper Promise handling', () => {
+  const locations = ['Lagos']
+  const weatherInfo = helpers.getWeatherInfo(locations);
+  expect(weatherInfo).toEqual([undefined]);
 });
