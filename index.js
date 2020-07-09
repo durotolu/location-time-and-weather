6b203c8be5c5f659597ab97f373ced2f85e3e3f3
Modurotolu Olokode <durolawk@gmail.com>
2020-07-09 17:25:35 +0100

    fixed no location provided bug

--- index.js
+++ index.js
@@ -3,45 +3,45 @@ const axios = require("axios");
 const helpers = require('./helpers')
 
 const [, , ...args] = process.argv;
-const locations = helpers.formatInputs([...args]);
 
-function getTimeWeather(locationArray) {
-  if (!locationArray) {
-    console.log("kindly provide location(s)");
+function getTimeWeather(input) {
+  if (!input[0]) {
+    console.log("kindly provide location(s), Usage => npm start LOCATIONS(seperated by commas(,), not spaces)");
   } else {
+    const locationArray = helpers.formatInputs([...input]);
     console.log("request processing...");
     const locationsData = helpers.getWeatherInfo(locationArray);
 
     axios.all(locationsData)
-    .then(axios.spread((...res) => {
-      res.forEach((location) => {
-        const locationName = location.data.name;
-        const currentTime = helpers.getTimeFromTimezone(location.data.timezone);
-        const description = location.data.weather[0].description;
-        const tempPressureHumidity = location.data.main;
-        const visibility = location.data.visibility;
-        const windSpeed = location.data.wind.speed;
-        const locationWeatherInfo = {
-          locationName,
-          currentTime,
-          weather: {
-            description,
-            ...tempPressureHumidity,
-            visibility,
-            windSpeed
-          }
+      .then(axios.spread((...res) => {
+        res.forEach((location) => {
+          const locationName = location.data.name;
+          const currentTime = helpers.getTimeFromTimezone(location.data.timezone);
+          const description = location.data.weather[0].description;
+          const tempPressureHumidity = location.data.main;
+          const visibility = location.data.visibility;
+          const windSpeed = location.data.wind.speed;
+          const locationWeatherInfo = {
+            locationName,
+            currentTime,
+            weather: {
+              description,
+              ...tempPressureHumidity,
+              visibility,
+              windSpeed
+            }
+          };
+          console.log(locationWeatherInfo);
+        });
+      })).catch(err => {
+        if (err.response) {
+          console.log({ 'seperate locations with commas (without spaces)': err.response.data.message });
+        } else {
+          console.log({ 'an error occured': 'kindly verify internet connectivity and try again' });
         };
-        console.log(locationWeatherInfo);
       });
-    })).catch(err => {
-      if (err.response) {
-        console.log({ 'seperate locations with commas (without spaces)': err.response.data.message });
-      } else {
-        console.log({ 'an error occured': 'kindly verify internet connectivity and try again' });
-      };
-    });
   };
 };
 
-getTimeWeather(locations);
+getTimeWeather(args);
 module.exports = getTimeWeather;
